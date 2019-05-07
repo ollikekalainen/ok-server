7db70fc4c580c8eff3a6faecdbf5b4fbcc91523f
Olli Kekäläinen <46929991+ollikekalainen@users.noreply.github.com>
2019-05-07 13:52:51 +0300

    Delete options-test.js

--- examples/server/options-test.js
+++
@@ -1,9 +0,0 @@
-const okserver = require( "ok-server" );
-const options = okserver.defaultOptions;
-options.port = 3099;
-options.defaultDirectory = "/sites/my-test";
-options.mimeTypes[".mp3"] = "audio/mpeg";
-options.mimeTypes[".wav"] = "audio/x-wav";
-okserver.createServer({
-    optionsFile: __dirname + "/options-test.json"
-});
